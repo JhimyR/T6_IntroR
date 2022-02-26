e411211e79533d17974be8f0e4b3598fa141ec55
JhimyR <jhimy.siquihua@est.ikiam.edu.ec>
2022-02-26 18:39:04 -0500

    Siquihua Jhimy subió un archivo R
    Resolución en R

---
+++ Tarea_ CiudadesT6.R
@@ -0,0 +1,15 @@
+## print("2022II GBI6 Bioinformática")
+# Tarea6_IntroR
+
+ciudad <- "Tena"
+paises_andes <- c("Argentina","Bolivia","Colombia","Chile","Ecuador","Peru","Venezuela")
+
+poblacion_paises_america <- data.frame(identador = seq(1:12),
+                                       norte.sur = as.factor(c(rep(0. 10), 1, 1))
+                                       paises = c("Argentina", "Bolivia","Brasil", "Colombia", "Chile", "Ecuador", "Perú", 
+                                       "Paraguay", "Surinam", "USA", "México"),
+                                       poblacion = c(45.38, 11.67, 212.6, 50.88, 19.12, 17.64, 32.97, 7,133,
+                                                     0,586, 329.5, 128.9)) #millones
+sewd("")
+
+ 
